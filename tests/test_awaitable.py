de1e8c2ac0d420d6606c56aa2cf6b9e4fd8b0519
Peter <49501366+ZeroIntensity@users.noreply.github.com>
2024-04-23 10:09:02 -0400

    Update test_awaitable.py

--- tests/test_awaitable.py
+++ tests/test_awaitable.py
@@ -1,2 +1,12 @@
 from ward import test
 import pyawaitable
+import ctypes as ct
+from ctypes import pythonapi
+
+get_pointer = PyCapsule_GetPointer
+get_pointer.argtypes = (ct.py_object, ct.c_void_p)
+get_pointer.restype = ct.c_void_p
+
+@test("awaitable creation")
+async def _():
+  ptr = get_pointer(pyawaitable._api)
