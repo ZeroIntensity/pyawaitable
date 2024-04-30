06ba1e20a4bacfcd676cbc2954585759506b77f8
Peter <49501366+ZeroIntensity@users.noreply.github.com>
2024-04-30 08:35:32 -0400

    Create __init__.py

---
+++ src/pyawaitable/__init__.py
@@ -0,0 +1,10 @@
+"""
+PyAwaitable - CPython API for asynchronous functions.
+
+Docs: https://awaitable.zintensity.dev/
+Source: https://github.com/ZeroIntensity/pyawaitable
+
+You probably don't need this module from Python! This module is for use from the C API.
+"""
+
+from . import abi
