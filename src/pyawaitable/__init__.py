cd2877c9044acd2370cc3ac3a182d8ebc3c1572b
Peter <49501366+ZeroIntensity@users.noreply.github.com>
2024-04-30 09:08:34 -0400

    Update __init__.py

--- src/pyawaitable/__init__.py
+++ src/pyawaitable/__init__.py
@@ -8,3 +8,9 @@ You probably don't need this module from Python! This module is for use from the
 """
 
 from . import abi
+from _pyawaitable import _awaitable
+from typing import Type
+
+__all__ = "abi", "Awaitable"
+
+Awaitable: Type = _awaitable
