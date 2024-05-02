e61048d88cf7245c1f8de2478121376bba22bdc5
Peter <49501366+ZeroIntensity@users.noreply.github.com>
2024-05-02 08:22:50 -0400

    Update genwrapper.h

--- include/pyawaitable/genwrapper.h
+++ include/pyawaitable/genwrapper.h
@@ -5,6 +5,13 @@
 
 extern PyTypeObject _AwaitableGenWrapperType;
 
+typedef struct {
+    PyObject_HEAD
+    PyObject *gw_result;
+    AwaitableObject *gw_aw;
+    PyObject *gw_current_await;
+} GenWrapperObject;
+
 PyObject *gen_next(PyObject *self);
 
 #endif
