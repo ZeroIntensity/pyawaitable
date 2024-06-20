1efe7c0f3175f85a5c38f18167312d7f14fcd0fa
ZeroIntensity <zintensitydev@gmail.com>
2024-06-19 23:27:52 -0400

    add (untested) uncrustify config

--- include/pyawaitable/genwrapper.h
+++ include/pyawaitable/genwrapper.h
@@ -2,26 +2,28 @@
 #define PYAWAITABLE_GENWRAPPER_H
 
 #include <Python.h>
+#include <pyawaitable/awaitableobject.h>
 
 extern PyTypeObject _AwaitableGenWrapperType;
 
 typedef struct {
     PyObject_HEAD
-    PyObject *gw_result;
-    AwaitableObject *gw_aw;
-    PyObject *gw_current_await;
+    PyObject* gw_result;
+    AwaitableObject* gw_aw;
+    PyObject* gw_current_await;
 } GenWrapperObject;
 
-PyObject *
-genwrapper_next(PyObject *self);
+PyObject*
+genwrapper_next(PyObject* self);
 
 void
-genwrapper_set_result(PyObject *gen, PyObject *result);
+genwrapper_set_result(PyObject* gen, PyObject* result);
 
 int
-genwrapper_fire_err_callback(PyObject *self, PyObject *await, awaitable_callback *cb);
+genwrapper_fire_err_callback(PyObject* self, PyObject* await,
+                             awaitable_callback* cb);
 
-PyObject *
-genwrapper_new(AwaitableObject *aw);
+PyObject*
+genwrapper_new(AwaitableObject* aw);
 
 #endif
