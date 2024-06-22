d599127526f8c0579628cd08f3e64e368c3ff550
ZeroIntensity <zintensitydev@gmail.com>
2024-06-22 13:22:28 -0400

    Use --no-build-isolation for the test package

--- tests/extension/setup.py
+++ tests/extension/setup.py
@@ -1,18 +1,14 @@
 from setuptools import Extension, setup
-import subprocess
-import site
-import os
+import pyawaitable
 
 if __name__ == "__main__":
-    subprocess.run(["pip", "install", "../../"])  # relative paths don't work in pyproject.toml
-
     setup(
         name="pyawaitable_test",
         ext_modules=[
             Extension(
                 "_pyawaitable_test",
                 sources=["./test.c", "./a.c"],
-                include_dirs=[os.path.join(site.getusersitepackages(), "include")],
+                include_dirs=[pyawaitable.include()],
             )
         ],
         license="MIT",
