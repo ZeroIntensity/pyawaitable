ee6c12b9fd84690fe10fb17b54b6605592a20ff6
ZeroIntensity <zintensitydev@gmail.com>
2024-06-22 13:07:01 -0400

    Use local site packages

--- tests/extension/setup.py
+++ tests/extension/setup.py
@@ -1,15 +1,18 @@
 from setuptools import Extension, setup
-import pyawaitable
 import subprocess
+import site
+import os
+
 if __name__ == "__main__":
     subprocess.run(["pip", "install", "../../"])  # relative paths don't work in pyproject.toml
+
     setup(
         name="pyawaitable_test",
         ext_modules=[
             Extension(
                 "_pyawaitable_test",
                 sources=["./test.c", "./a.c"],
-                include_dirs=[pyawaitable.include()],
+                include_dirs=[os.path.join(site.getusersitepackages(), "include")],
             )
         ],
         license="MIT",
