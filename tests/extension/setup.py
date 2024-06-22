713236c9abce859c98e54e1d38876873a95bc404
ZeroIntensity <zintensitydev@gmail.com>
2024-06-22 12:59:58 -0400

    Use local subprocess call to install project

--- tests/extension/setup.py
+++ tests/extension/setup.py
@@ -1,19 +1,15 @@
 from setuptools import Extension, setup
-import site
-import os
-
-# pyproject.toml doesn't let us specify the local pyawaitable
-# build as a requirement, so `pip install` fails when trying to
-# import pyawaitable
-
+import pyawaitable
+import subprocess
 if __name__ == "__main__":
+    subprocess.run(["pip", "install", "../../"])  # relative paths don't work in pyproject.toml
     setup(
         name="pyawaitable_test",
         ext_modules=[
             Extension(
                 "_pyawaitable_test",
                 sources=["./test.c", "./a.c"],
-                include_dirs=[os.path.join(site.getusersitepackages(), "pyawaitable")],
+                include_dirs=[pyawaitable.include()],
             )
         ],
         license="MIT",
