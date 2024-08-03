bd525b2f428311d8ea673ba7e79b21de6bccb588
ZeroIntensity <zintensitydev@gmail.com>
2024-08-03 16:11:40 -0400

    Fix leak in backport of PyErr_SetRaisedException

--- src/_pyawaitable/backport.c
+++ src/_pyawaitable/backport.c
@@ -41,7 +41,7 @@ PyErr_GetRaisedException(void)
 void
 PyErr_SetRaisedException(PyObject *err)
 {
-    PyErr_Restore(Py_NewRef((PyObject *) Py_TYPE(err)), err, NULL);
+    PyErr_Restore(Py_TYPE(err), err, NULL);
 }
 
 #endif
