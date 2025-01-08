88d5345c6c85778c889a1c797dd266317490ed11
Peter Bierma <zintensitydev@gmail.com>
2025-01-07 22:24:58 -0500

    Fix platform-dependent builds.

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from glob import glob
 from setuptools import Extension, setup
 import os
 
+DEBUG_SYMBOLS = "/DEBUG" if os.name == "nt" else "-g"
+_OPTIMIZED = "/O3" if os.name == "nt" else "-O3"
+_NO_OPTIMIZATION = "" if os.name == "nt" else "-O0"
+OPTIMIZATION = _OPTIMIZED if os.environ.get("PYAWAITABLE_OPTIMIZED") else _NO_OPTIMIZATION
+
 if __name__ == "__main__":
     setup(
         name="pyawaitable",
@@ -13,7 +18,7 @@ if __name__ == "__main__":
                 "_pyawaitable",
                 glob("./src/_pyawaitable/*.c"),
                 include_dirs=["./include/", "./src/pyawaitable/"],
-                extra_compile_args=["-g", "-O3" if os.environ.get("PYAWAITABLE_OPTIMIZED") else "-O0"],
+                extra_compile_args=[DEBUG_SYMBOLS, OPTIMIZATION],
             )
         ],
         package_dir={"": "src"},
