1f7c2539c978b35293c6e21533a6e388143730c9
Peter Bierma <zintensitydev@gmail.com>
2025-01-10 19:25:55 -0500

    Reformat some header files.

--- include/pyawaitable/backport.h
+++ include/pyawaitable/backport.h
@@ -2,6 +2,26 @@
 #define PYAWAITABLE_BACKPORT_H
 
 #include <Python.h>
+#include <pyawaitable/dist.h>
+
+#ifndef Py_NewRef
+static inline PyObject *
+_PyAwaitable_NO_MANGLE(Py_NewRef)(PyObject *o)
+{
+    Py_INCREF(o);
+    return o;
+}
+
+#endif
+
+#ifndef Py_XNewRef
+static inline PyObject *
+_PyAwaitable_NO_MANGLE(Py_XNewRef)(PyObject *o)
+{
+    Py_XINCREF(o);
+    return o;
+}
+#endif
 
 #if PY_VERSION_HEX < 0x030c0000
 static PyObject *
@@ -22,25 +42,6 @@ _PyAwaitable_NO_MANGLE(PyErr_SetRaisedException)(PyObject *err)
     // NOTE: We need to incref the type object here, even though
     // this function steals a reference to err.
     PyErr_Restore(Py_NewRef((PyObject *) Py_TYPE(err)), err, NULL);
-}
-#endif
-
-#ifndef Py_NewRef
-static inline PyObject *
-_PyAwaitable_NO_MANGLE(Py_NewRef)(PyObject *o)
-{
-    Py_INCREF(o);
-    return o;
-}
-
-#endif
-
-#ifndef Py_XNewRef
-static inline PyObject *
-_PyAwaitable_NO_MANGLE(Py_XNewRef)(PyObject *o)
-{
-    Py_XINCREF(o);
-    return o;
 }
 #endif
 
--- include/pyawaitable/values.h
+++ include/pyawaitable/values.h
@@ -2,26 +2,80 @@
 #define PYAWAITABLE_VALUES_H
 
 #include <Python.h> // PyObject, Py_ssize_t
+#include <pyawaitable/dist.h>
 
-// Normal values
+/* Object values */
 
-_PyAwaitable_API(int) PyAwaitable_SaveValues(PyObject * awaitable, Py_ssize_t nargs, ...);
-_PyAwaitable_API(int) PyAwaitable_UnpackValues(PyObject * awaitable, ...);
-_PyAwaitable_API(int) PyAwaitable_SetValue(PyObject * awaitable, Py_ssize_t index, PyObject * new_value);
-_PyAwaitable_API(PyObject *) PyAwaitable_GetValue(PyObject * awaitable, Py_ssize_t index);
+_PyAwaitable_API(int)
+PyAwaitable_SaveValues(
+    PyObject * awaitable,
+    Py_ssize_t nargs,
+    ...
+);
 
-// Arbitrary values
+_PyAwaitable_API(int)
+PyAwaitable_UnpackValues(PyObject * awaitable, ...);
 
-_PyAwaitable_API(int) PyAwaitable_SaveArbValues(PyObject * awaitable, Py_ssize_t nargs, ...);
-_PyAwaitable_API(int) PyAwaitable_UnpackArbValues(PyObject * awaitable, ...);
-_PyAwaitable_API(int) PyAwaitable_SetArbValue(PyObject * awaitable, Py_ssize_t index, void * new_value);
-_PyAwaitable_API(void *) PyAwaitable_GetArbValue(PyObject * awaitable, Py_ssize_t index);
+_PyAwaitable_API(int)
+PyAwaitable_SetValue(
+    PyObject * awaitable,
+    Py_ssize_t index,
+    PyObject * new_value
+);
+_PyAwaitable_API(PyObject *)
+PyAwaitable_GetValue(
+    PyObject * awaitable,
+    Py_ssize_t index
+);
 
-// Integer values
+/* Arbitrary values */
 
-_PyAwaitable_API(int) PyAwaitable_SaveIntValues(PyObject * awaitable, Py_ssize_t nargs, ...);
-_PyAwaitable_API(int) PyAwaitable_UnpackIntValues(PyObject * awaitable, ...);
-_PyAwaitable_API(int) PyAwaitable_SetIntValue(PyObject * awaitable, Py_ssize_t index, long new_value);
-_PyAwaitable_API(long) PyAwaitable_GetIntValue(PyObject * awaitable, Py_ssize_t index);
+_PyAwaitable_API(int)
+PyAwaitable_SaveArbValues(
+    PyObject * awaitable,
+    Py_ssize_t nargs,
+    ...
+);
+
+_PyAwaitable_API(int)
+PyAwaitable_UnpackArbValues(PyObject * awaitable, ...);
+
+_PyAwaitable_API(int)
+PyAwaitable_SetArbValue(
+    PyObject * awaitable,
+    Py_ssize_t index,
+    void *new_value
+);
+
+_PyAwaitable_API(void *)
+PyAwaitable_GetArbValue(
+    PyObject * awaitable,
+    Py_ssize_t index
+);
+
+/* Integer values */
+
+_PyAwaitable_API(int)
+PyAwaitable_SaveIntValues(
+    PyObject * awaitable,
+    Py_ssize_t nargs,
+    ...
+);
+
+_PyAwaitable_API(int)
+PyAwaitable_UnpackIntValues(PyObject * awaitable, ...);
+
+_PyAwaitable_API(int)
+PyAwaitable_SetIntValue(
+    PyObject * awaitable,
+    Py_ssize_t index,
+    long new_value
+);
+
+_PyAwaitable_API(long)
+PyAwaitable_GetIntValue(
+    PyObject * awaitable,
+    Py_ssize_t index
+);
 
 #endif
