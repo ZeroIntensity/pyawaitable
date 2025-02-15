51ed01029a73314f32a88901a6dc99aab45e196a
Peter Bierma <zintensitydev@gmail.com>
2025-02-15 12:35:57 -0500

    Fix build failures.

--- include/pyawaitable/with.h
+++ include/pyawaitable/with.h
@@ -2,14 +2,14 @@
 #define PYAWAITABLE_WITH_H
 
 #include <Python.h> // PyObject
-#include <pyawaitable/awaitableobject.h> // awaitcallback, awaitcallback_err
+#include <pyawaitable/awaitableobject.h> // PyAwaitable_Callback, PyAwaitable_Error
 
 _PyAwaitable_API(int)
 PyAwaitable_AsyncWith(
     PyObject * aw,
     PyObject * ctx,
-    awaitcallback cb,
-    awaitcallback_err err
+    PyAwaitable_Callback cb,
+    PyAwaitable_Error err
 );
 
 #endif
