14fcf4b6c5b4639482b9fcf3730dfa2f0ae75574
Peter Bierma <zintensitydev@gmail.com>
2025-03-30 09:59:51 -0400

    Add some debugging to the script.

--- tests/builds/ensure_build_worked.py
+++ tests/builds/ensure_build_worked.py
@@ -1,13 +1,39 @@
+"""
+Script to ensure that an extension module containing PyAwaitable was successfully built.
+Intended to be run by Hatch in CI.
+"""
 import optparse
 import sys
 import asyncio
 import traceback
 import importlib
+import site
+import pyawaitable
+import os
+from typing import TypeVar
 
+T = TypeVar("T")
+
+def not_none(value: T | None) -> T:
+    assert value is not None, "got None where None shouldn't be"
+    return value
+
+def debug_directory(what: str, path: str) -> None:
+    print(f"{what}: {path}", file=sys.stderr)
+    print(f"  Contents of {what}:", file=sys.stderr)
+    for root, _, files in os.walk(path):
+        for file in files:
+            print(f"  {what}: {os.path.join(root, file)}", file=sys.stderr)
+
+def debug_import_error(error: ImportError) -> None:
+    debug_directory("PyAwaitable Include", pyawaitable.include())
+    debug_directory("User Site", not_none(site.USER_SITE))
+    debug_directory("User Base", not_none(site.USER_BASE))
+    print(error)
 
 def main():
     parser = optparse.OptionParser()
-    opts, args = parser.parse_args()
+    _, args = parser.parse_args()
 
     if not args:
         parser.error("Expected one argument.")
@@ -24,7 +50,11 @@ def main():
         asyncio.run(importlib.import_module(mod).async_function(dummy()))
     except BaseException as err:
         traceback.print_exc()
-        print("Build failed!", file=sys.stderr)
+        if isinstance(err, ImportError):
+            debug_import_error(err)
+            print("Failed to import the module!", file=sys.stderr)
+        else:
+            print("Build failed!", file=sys.stderr)
         sys.exit(1)
     
     if not called:
